59dfa474c4076b8d7ff8c8d2feae7bc4562708dd
Cameron <cm.mullaney17@gmail.com>
2026-04-30 13:04:54 -0700

    Final - Markdown & Formatting edits
    Yep! Final version! Yippeeeee!

--- Finalproj_stream.py
+++ Finalproj_stream.py
@@ -5,13 +5,13 @@ fulldata = pd.read_csv("power_streaming_data.csv")
 
 ## And now, the loop!
 
-## This loop will select 5 rows from "power_streaming_data.csv" with replacement, and output them to the "power_stream" folder. 
+## This loop will select 5 rows from "power_streaming_data.csv" with replacement, and output them (as a csv) to the "power_stream" folder. 
 ## This will be carried out 20 times, with a 15 second break between iterations.
 for i in range(20):
+    
     fulldata.sample(5)\
-        .to_csv(f"power_stream/batch_{i}.csv", 
-                
-                index = False)
+        .to_csv(f"power_stream/batch_{i}.csv", index = False)
+    
     print("Batch", i, "sent!")
     
     time.sleep(15)
